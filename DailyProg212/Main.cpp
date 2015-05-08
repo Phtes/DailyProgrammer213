0f846a8fad4a4607e99c495fddcb4559aceb7b31
Kasey Klein <kasey@zookacreative.com>
2015-05-08 09:51:05 -0700

    Cleaned up working code used better output methods

--- DailyProg212/Main.cpp
+++ DailyProg212/Main.cpp
@@ -11,38 +11,25 @@ int main()
 	std::vector<char> vowels(vowel, vowel+12); // allocate mem
 	std::sort(vowels.begin(), vowels.end()); // prep map for binary search
 	std::string input;
-	std::string finalOutput;
 
 	std::cout << "Enter a sentence and watch it encode!" << std::endl;
 	
-	std::getline(std::cin, input); // Get entire line as input
+	std::getline(std::cin, input);
 
 	clock_t pStart = clock();
-	// Pass input to function that checks for consonants and adds to the string
-		for (int i = 0; i < input.length(); i++)
+
+	for (int i = 0; i < input.length(); i++)
+	{
+		printf("%c", input[i]);
+		if (std::binary_search(vowels.begin(), vowels.end(), input[i]) == false && isalpha(input[i]))
 		{
-			
-			if (std::binary_search(vowels.begin(), vowels.end(), input[i]) == true && isalpha(input[i]) && input[i] != input[i+1])
-			{
-				std::cout << "found" << std::endl;
-				finalOutput.push_back(input[i]);
-			}
-			else
-			{
-				std::cout << "Not found" << std::endl;
-				finalOutput.push_back(input[i]);
-				finalOutput.push_back('o');
-				finalOutput.push_back(tolower(input[i]));
-			}
+			printf("o%c", tolower(input[i]));
 		}
-
-	std::cout << finalOutput << std::endl;
-
+	}
+	std::cout << std::endl;
 	printf("Time taken: %.5fs\n", (double)(clock() - pStart) / CLOCKS_PER_SEC);
 
 	system("pause");
-	// return string
-	
 	
 	return 0;
 }
